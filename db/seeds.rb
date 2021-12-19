61ecc1571a2ee23f7f033f4f243dbc828afa20da
Douglas Feitosa <douglas_fg@hotmail.com>
2021-12-19 12:31:32 -0300

    feature/add_invoice_to_seed: Add invoice in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,12 @@
-user = User.create(email: 'invoice@invoice.com')
+user = User.create!(email: 'invoice@invoice.com')
 user.update(token: 'd5fd41ca-b44b-4d18-b7db-8f0d2cb0140e')
+
+Invoice.create!(
+  user: user,
+  number: '1234567489',
+  date: DateTime.new(2021, 12, 18),
+  company: 'Husky',
+  billing_for: 'DFG',
+  total: 10.0,
+  emails: 'douglasfeitosa@outlook.com'
+)
